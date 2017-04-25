c9619a40f02b8f03d096a02d92e2dcd55f68c1e5
edris1s <zoom_edo@yahoo.com>
2017-04-25 17:42:07 -0500

    this is twitter bot apps

--- edris2/config.js
+++ edris2/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    consumer_key: 'no02xWr3tXdclCDmU3IgXKABv',
-    consumer_secret: 'RLmLdHb7MWo8WxlzooZMFYdSS2w1PL8RMUOretpZiQwtHPsZts',
-    access_token: '753788906-XtzZ15M3C0XEmMorRPegW145sCbqT4HZN1QsgNAo',
-    access_token_secret: 'Bi9BtVgWp7sIvRDn7sQ86bYyL9bViZmXrepcbBTepQVG3'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
 };
